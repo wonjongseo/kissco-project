62555559c048284ef3dd97d7b26e8a76942bf6d4
wonjongseo <visionwill@naver.com>
2022-09-11 01:24:44 +0900

    fix beans

--- src/main/java/com/kissco/kisscodic/remote/repository/AwsConfig.java
+++ src/main/java/com/kissco/kisscodic/remote/repository/AwsConfig.java
@@ -9,26 +9,20 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
 public class AwsConfig {
 
-    @Value("${cloud.aws.credentials.accessKey}")
-    private String accessKey;
+    private String accessKey ="";
 
 
-    @Value("${cloud.aws.credentials.secretKey}")
-    private String secretKey;
+
+    private String secretKey="";;
 
 
-    @Value("${cloud.aws.region.static}")
-    private String region;
+
+    private String region="";;
 
 
-    @Bean
     public AmazonS3 amazonS3() {
-        System.out.println("secretKey = " + secretKey);
-        System.out.println("accessKey = " + accessKey);
-        System.out.println("region = " + region);
         AWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
 
         return AmazonS3ClientBuilder.standard().withRegion(region)
